036b81096f9ae652a0d2598a70a871d8c59b0f3c
Aryak <pandeary12@gmail.com>
2021-03-27 16:40:35 -0400

    almost done with non gui code

--- src/cs213p4/Flavor.java
+++ src/cs213p4/Flavor.java
@@ -1,10 +1,31 @@
 package cs213p4;
 
 public enum Flavor {
-    CHOCOLATE_FROSTED,STRAWBERRY,GLAZED,BOSTON_CREAM,JELLY_FILLED,BLUEBERRY,POWDERED_SUGAR,WHOLE_WHEAT,CINNAMON;
+    CHOCOLATE_FROSTED,STRAWBERRY,GLAZED,BOSTON_CREAM,JELLY_FILLED,BLUEBERRY,POWDERED_SUGAR,WHOLE_WHEAT,MAPLE_SYRUP_FROSTED;
 
     @Override
     public String toString() {
-        return super.toString();
+        switch(this){
+            case CHOCOLATE_FROSTED:
+                return "Choc Frosted";
+            case GLAZED:
+                return "Glazed";
+            case MAPLE_SYRUP_FROSTED:
+                return "Map Syrp Frstd";
+            case BLUEBERRY:
+                return "Blueberry";
+            case STRAWBERRY:
+                return "Strawberry";
+            case WHOLE_WHEAT:
+                return "Whole Wheat";
+            case BOSTON_CREAM:
+                return "Bstn Cream";
+            case JELLY_FILLED:
+                return "Jelly";
+            case POWDERED_SUGAR:
+                return "Sugar";
+            default:
+                return "invalid";
+        }
     }
 }
