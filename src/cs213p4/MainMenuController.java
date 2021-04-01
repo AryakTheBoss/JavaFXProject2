58fdf4b9a0510eec1785732e0f55e9ecc0c8e53c
Mayank Singamreddy <singamreddy.mayank@gmail.com>
2021-04-01 14:14:54 -0400

    main menu done

--- src/cs213p4/MainMenuController.java
+++ src/cs213p4/MainMenuController.java
@@ -1,4 +1,74 @@
 package cs213p4;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainMenuController{
+
+
+    @FXML
+
+    public void orderCoffee(){
+        Parent root = null;
+        Stage newWindow = new Stage();
+        try { //open main menu
+            root = FXMLLoader.load(getClass().getResource("OrderCoffee.fxml"));
+            newWindow.setTitle("Order Coffee");
+            newWindow.setScene(new Scene(root, 590, 475));
+            newWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+
+    public void orderDonuts(){
+        Parent root = null;
+        Stage newWindow = new Stage();
+        try { //open main menu
+            root = FXMLLoader.load(getClass().getResource("OrderDonuts.fxml"));
+            newWindow.setTitle("Order Donuts");
+            newWindow.setScene(new Scene(root, 590, 475));
+            newWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+
+    public void myOrder(){
+        Parent root = null;
+        Stage newWindow = new Stage();
+        try { //open main menu
+            root = FXMLLoader.load(getClass().getResource("MyOrder.fxml"));
+            newWindow.setTitle("My Order");
+            newWindow.setScene(new Scene(root, 590, 475));
+            newWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+
+    public void storeOrders(){
+        Parent root = null;
+        Stage newWindow = new Stage();
+        try { //open main menu
+            root = FXMLLoader.load(getClass().getResource("StoreOrders.fxml"));
+            newWindow.setTitle("Store Orders");
+            newWindow.setScene(new Scene(root, 590, 475));
+            newWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
